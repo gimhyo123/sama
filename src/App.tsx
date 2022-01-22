2f0dc7ecc6d5b128446999d293c5865d9fab95ca
gimyo <gimh81040@gmail.com>
2022-01-22 12:14:59 +0900

    레이아웃 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import Sidebar from './Components/Sidebar/Sidebar';
+import Summary from './Components/Summary';
+import "./index.css"
 
 function App() {
   return (
     <div className="App">
-      
+      <Sidebar/>
+      <section>
+        <Summary/>
+      </section>
     </div>
   );
 }
