ed76c8467e0d2c9a1fc17623932a0a1b4209174e
gimyo <gimh81040@gmail.com>
2022-01-22 12:39:42 +0900

    박스 컴포넌트 생성

---
+++ src/Components/Box/Box.tsx
@@ -0,0 +1,18 @@
+import { FunctionComponent } from "react"
+import "./style.css"
+
+interface PropsType {
+    isPadding?: boolean
+}
+
+const Box: FunctionComponent<PropsType> = (props) => {
+    return (
+        <div className="box"  style={{
+            padding: props.isPadding ? '15px': 0
+        }}>
+            {props.children}
+        </div>
+    )
+}
+
+export default Box
--- src/Components/Sidebar/Sidebar.tsx
+++ src/Components/Sidebar/Sidebar.tsx
@@ -1,19 +1,25 @@
+import Box from "../Box"
 import "./style.css"
 
 
 const Sidebar = () => {
     return (
         <aside>
-            <header>
-                <h1>
-                    CORONA LIVE
-                </h1>
-            </header>
+            <Box isPadding>
+                <header>
+                    <h1>
+                        CORONA LIVE
+                    </h1>
+                </header>
+            </Box>
+
+            <Box >
             <ul>
                 <li className="selected">국내</li>
                 <li>해외</li>
                 <li>백신</li>
             </ul>
+            </Box>
         </aside>
     )
 }
