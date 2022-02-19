b2a6f38c3864c0a7aab4f405c373c654f94cb931
gimyo <gimh81040@gmail.com>
2022-02-19 09:52:38 +0900

    테이블 정렬 추가

---
+++ src/Components/SortArrow/SortArrow.tsx
@@ -0,0 +1,22 @@
+import { FunctionComponent } from "react"
+import "./style.css"
+
+interface Props {
+    sortKey: string
+    sortOption: {
+        key: string
+        order: 'asc' | "desc"
+    }
+}
+const SortArrow: FunctionComponent<Props> = props => {
+    if(props.sortKey ! == props.sortOption.key) return null
+
+    return (
+        <span className="material-icons">
+            {props.sortOption.order == 'asc' ? 'arrow_upward' : 'arrow_downward'}
+
+        </span>
+    )
+}
+
+export default SortArrow
